30184ef09ac3e0af6f832269f5f195d15d0e2dea
dalyd14 <dalyd14@gmail.com>
2021-03-25 18:26:32 -0400

    graphql server has been set up and tested using the playground

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, username: String!, password: String!): Auth
+        addBook(authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
+        removeBook(bookId: String!): User
+    }
+`
+
+module.exports = typeDefs
